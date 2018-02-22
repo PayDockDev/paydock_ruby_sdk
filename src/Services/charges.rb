00f1bb164cdd6a5d61f86f1d73a5b857027502f0
Pushkar Kadam <pushkar.kadam@paydock.com>
2018-02-22 16:39:29 +1100

    Added reference and description fields to Stripe connection charge

--- src/Services/charges.rb
+++ src/Services/charges.rb
@@ -130,10 +130,12 @@ module PayDock
 		end
 
 		# TODO: this isn't right. We need to support each of the different transfer options for stripe connect, not just 2. We also need to support more than just using a token
-		def self.create_stripe_connection_charge(amount,currency,stripe_transfer_group_id, token, amount1:0,stripe_account_id_1:"",amount2:0,stripe_account_id_2:"",stripe_direct_account_id:nil,stripe_application_fee:nil,stripe_destination_account_id:nil,stripe_destination_amount:nil,securepay_fraud_guard:nil,ip_address:nil)
+		def self.create_stripe_connection_charge(amount,currency,stripe_transfer_group_id, token,reference:nil,description:nil, amount1:0,stripe_account_id_1:"",amount2:0,stripe_account_id_2:"",stripe_direct_account_id:nil,stripe_application_fee:nil,stripe_destination_account_id:nil,stripe_destination_amount:nil,securepay_fraud_guard:nil,ip_address:nil)
 			body = {
 				:amount => amount,
 				:currency => currency,
+				:reference => reference,
+				:description => description,
 				:token => token,
 				:metadata => {
 					:stripe_direct_account_id => stripe_direct_account_id,
